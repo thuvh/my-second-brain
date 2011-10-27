e65b3ae4a391367d69917638c3bc9cf3fccf9820
tantrieuf31@gmail.com <tantrieuf31@gmail.com@3c568036-8cb4-dbdd-547e-6f390834191f>
2011-10-27 11:31:08 +0000

    mongo db

--- trunk/test-cases/org/brain2/test/utils/XPathUtil.java
+++ trunk/test-cases/org/brain2/test/utils/XPathUtil.java
@@ -10,6 +10,11 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.w3c.dom.Node;
 
+import com.mongodb.CommandResult;
+import com.mongodb.DB;
+import com.mongodb.Mongo;
+import com.mongodb.MongoException;
+
 import de.l3s.boilerpipe.extractors.ArticleExtractor;
 
 public class XPathUtil {
@@ -22,15 +27,15 @@ public class XPathUtil {
 			child = child.getNextSibling();
 		}
 	}
-	
-	private static boolean valid(String s) {		
+
+	private static boolean valid(String s) {
 		if (s.matches("javascript:.*|mailto:.*|#.*")) {
 			return false;
 		}
 		return true;
 	}
 
-	public static void main(String[] args)  {
+	public static void main(String[] args) {
 
 		String url = "http://vnexpress.net/gl/vi-tinh/san-pham-moi/2011/10/dien-thoai-la-voi-man-hinh-uon-cong-cua-nokia/";
 		String html = LinkGetter.loadHtmlPage(url);
@@ -38,76 +43,103 @@ public class XPathUtil {
 
 		Document doc = Jsoup.parse(html);
 		doc.setBaseUri("http://vnexpress.net");
-		
+
 		Elements feedDateNodes = doc.select("div.TurnTopPage");
 		System.out.println(feedDateNodes.size());
 		for (Element feedDateNode : feedDateNodes) {
 			System.out.println(feedDateNode.attr("class"));
 			System.out.println(feedDateNode.text());
 		}
-		
-		Elements links = doc.select("a[href]");// a with href		
+
+		Elements links = doc.select("a[href]");// a with href
 		for (Element link : links) {
 			String href = link.attr("href");
-			if(valid(href)){				
-				if(!href.startsWith("http")){
-					href = "http://vnexpress.net" + href;					
+			if (valid(href)) {
+				if (!href.startsWith("http")) {
+					href = "http://vnexpress.net" + href;
 				}
 				System.out.println(href);
 				try {
 					URL urlObj = new URL(href);
 					System.out.println(urlObj.getHost());
-				} catch (Exception e) {	}
+				} catch (Exception e) {
+				}
 			}
 		}
-		
-		Elements contents = doc.select("div");// a with href		
+
+		Elements contents = doc.select("div");// a with href
 		for (Element content : contents) {
 			System.out.println(content.html());
 		}
-		try {		
+		try {
 
 			// This can also be done in one line:
 			System.out.println(ArticleExtractor.INSTANCE.getText(html));
-		}  catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		try {
-			final Process p = new ProcessBuilder("java", "-jar", "D:/Researchs/JsOptimizer/dist/JsOptimizer.jar").start();
-			
-			new Thread(new Runnable() {public void run() {
-				  try {
-					IOUtils.copy(p.getInputStream(), System.out);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				} } ).start();
-			
-		
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-		
-		return;
-		
-//		try {
-//			DOMFragmentParser parser = new DOMFragmentParser();
-//			HTMLDocument document = new HTMLDocumentImpl();
-//
-//			DocumentFragment fragment = document.createDocumentFragment();
-//			parser.parse(url, fragment);			
-//			print(fragment, "");
-//			
-//			
-//
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
+		try {
+			// final Process p = new ProcessBuilder("java", "-jar",
+			// "D:/Researchs/JsOptimizer/dist/JsOptimizer.jar").start();
 
+			boolean ok = false;
+			try {
+				Mongo m = new Mongo("127.0.0.1");
+				if (!m.isLocked()) {
+					ok = true;		
+
+					System.out.println("mongod has started");
+					Thread.sleep(2000);
+					DB db = m.getDB("admin");
+					CommandResult rs = db.command("shutdown", 1);
+					System.out.println(rs);
+				}
+			} catch (MongoException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			//System.exit(1);
+
+			if (!ok) {
+				final Process p = new ProcessBuilder(
+						"D:/Researchs/mongodb-win32-i386-2.0.1/bin/mongod",
+						"--dbpath",
+						"D:/Researchs/mongodb-win32-i386-2.0.1/data/db")
+						.start();
+
+				new Thread(new Runnable() {
+					public void run() {
+						try {
+							IOUtils.copy(p.getInputStream(), System.out);
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}).start();
+			} else {				
+				System.out.println("mongod has started");
+				while (ok) {
+					Thread.sleep(2000);
+					System.out.println("working ...");
+					
+					
+				}
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		Thread hook = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				System.out.println("exit ....");
+			}
+		});
+		Runtime.getRuntime().addShutdownHook(hook);
 	}
 }
